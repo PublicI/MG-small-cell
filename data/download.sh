babe482bcacfb82a44413c8a12599f4920151828
Pratheek Rebala <pratheekrebala@gmail.com>
2018-01-11 14:53:10 -0500

    awesome sqlite3
    Signed-off-by: Pratheek Rebala <pratheekrebala@gmail.com>

--- data/download.sh
+++ data/download.sh
@@ -1,5 +1,6 @@
 wget https://www2.census.gov/geo/tiger/TIGER_DP/2015ACS/ACS_2015_5YR_BG_24.gdb.zip -O ACS_2015_5YR_BG_24.gdb.zip
 rm ACS_5yr.sqlite
 ogr2ogr --debug ON -dsco SPATIALITE=YES -f "SQLite" ACS_5yr.sqlite ACS_2015_5YR_BG_24.gdb.zip ACS_2015_5YR_BG_24_MARYLAND X01_AGE_AND_SEX X22_FOOD_STAMPS X19_INCOME X17_POVERTY
+ogr2ogr -update -append -f SQLite ACS_5yr.sqlite -nln "broadband" montgomery_county_speeds.csv
 rm ACS.geojson
 ogr2ogr --debug ON -f "GeoJSON" ACS.geojson ACS_5yr.sqlite -sql "$(cat ./subset.sql)"
--- data/subset.sql
+++ data/subset.sql
@@ -1,21 +1,29 @@
-select
-    aas.b01001e1 as totalpopulation,
-    aas.b01001m1 as totalpopulationerr,
-    fs.b22010e1 as foodstamps,
-    fs.b22010m1 as foodstampserr,
-    inc.b19013e1 as medianincome,
-    inc.b19013m1 as medianincomeerr,
-    pov.b17010e1 as povertystatus,
-    pov.b17010m1 as povertystatuserr,
-    aas.b01001e1/meta.aland as popDensity,
-    meta.*
-from acs_2015_5yr_bg_24_maryland meta
-join x01_age_and_sex aas
-    on meta.geoid_data=aas.geoid
-join x22_food_stamps fs
-    on meta.geoid_data=fs.geoid
-join x19_income inc
-    on meta.geoid_data=inc.geoid
-join x17_poverty pov
-    on meta.geoid_data=pov.geoid
-where meta.countyfp = "031"
+SELECT aas.b01001e1              AS totalpopulation, 
+       aas.b01001m1              AS totalpopulationerr, 
+       fs.b22010e1               AS foodstamps, 
+       fs.b22010m1               AS foodstampserr, 
+       inc.b19013e1              AS medianincome, 
+       inc.b19013m1              AS medianincomeerr, 
+       pov.b17010e1              AS povertystatus, 
+       pov.b17010m1              AS povertystatuserr, 
+       aas.b01001e1 / meta.aland AS popDensity, 
+       meta.*, 
+       brd.* 
+FROM   acs_2015_5yr_bg_24_maryland meta 
+       JOIN x01_age_and_sex aas 
+         ON meta.geoid_data = aas.geoid 
+       JOIN x22_food_stamps fs 
+         ON meta.geoid_data = fs.geoid 
+       JOIN x19_income inc 
+         ON meta.geoid_data = inc.geoid 
+       JOIN x17_poverty pov 
+         ON meta.geoid_data = pov.geoid 
+       JOIN (SELECT Substr(blockcode, 0, 13) AS blockgroup, 
+                    Count(DISTINCT frn)      AS numProviders, 
+                    Avg(maxaddown)           AS avgDown, 
+                    Avg(maxadup)             AS avgUp 
+             FROM   broadband 
+             WHERE  consumer = 1 
+             GROUP  BY blockgroup) brd 
+         ON meta.geoid = brd.blockgroup 
+WHERE  meta.countyfp = "031" 
